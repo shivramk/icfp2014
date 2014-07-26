c871a9ddfab5420ed23b4780d69fd0e277810ab6
Atmaram Shetye <atmaram.shetye@gmail.com>
2014-07-26 13:40:22 +0530

    Dumb logic v2

--- AI/sample.py
+++ AI/sample.py
@@ -1,8 +1,22 @@
 # Representing AI state as a list with the following indices
+FUNCTIONS = 0
+
+GLOBALS = 1
 TICKS = 0
-SCORE = 1
-HEIGHT = 2
-WIDTH = 3
+HEIGHT = 1
+WIDTH = 2
+
+TIMERS = 2
+LAMBDATICKSTIMER = 0
+LAMBDAEATTIMER = 1
+GHOST1TICKSTIMER = 2
+GHOST2TICKSTIMER = 3
+GHOST3TICKSTIMER = 4
+GHOST4TICKSTIMER = 5
+GHOST1FRIGHTTIMER = 6
+GHOST2FRIGHTTIMER = 7
+GHOST3FRIGHTTIMER = 8
+GHOST4FRIGHTTIMER = 9
 
 # Map Encoding
 WALL = 0
@@ -12,6 +26,7 @@ POWERPILL = 3
 FRUIT = 4
 LAMBDAMAN = 5
 GHOST = 6
+LAMBDAGHOST = 7
 
 
 def getNewPosition(oldPos, direction):
@@ -23,12 +38,13 @@ def getNewPosition(oldPos, direction):
         return (oldPos[0] + 1, oldPos[1])
     return (oldPos[0] - 1, oldPos[1])
 
+"""
 def fruit1Valid(ticks):
     return 127 * 200 <= ticks < 127 * 280
 
 def fruit2Valid(ticks):
     return 127 * 400 <= ticks < 127 * 480
-
+"""
 
 ######## Sequence in which map should be updated ######
 def lambdaGhostMove(currentAIState):
@@ -50,43 +66,45 @@ def checkLambdaLoss(currentAIState):
     return currentAIState
 
 def incrementTick(currentAIState):
-    ticks = currentAIState[TICKS] + 1
+    ticks = currentAIState[GLOBALS][TICKS] + 1
     return [ticks] + currentAIState[1:]
 
-Sequence = (lambdaGhostMove, actions, eat, dealWithGhosts,
+t_Sequence = (lambdaGhostMove, actions, eat, dealWithGhosts,
         checkLambdaWin, checkLambdaLoss, incrementTick)
 
 def makeMove(currentAIState):
     def f(x, y):
         return y(x) 
-    return reduce(f, Sequence, currentAIState)
+    return reduce(f, t_Sequence, currentAIState)
 
-def parse(worldMap):
-    ret = []
-    lambdaPos = None
-    for y in range(len(worldMap)):
-        for x in range(len(worldMap[0])):
-            if worldMap[y][x] == LAMBDAMAN:
-                lambdaPos = (x, y) # No break, also need to change loops
-    ret.append(lambdaPos)
-    return ret
-            
-            
-def getBestMove(currentAIState, currentWorldState):
-    worldMap = currentWorldState[0]
-    lambdaStatus = currentWorldState[1]
-    x, y = lambdaStatus[1]
+def getBestMove(currentAIState, t_currentWorldState):
+    worldMap = t_currentWorldState[0]
+    t_lambdaStatus = t_currentWorldState[1]
+    print t_currentWorldState
+    x, y = t_lambdaStatus[1]
     # Dumb logic, always returns first valid move from the sequence (urdl)
+    legal = 0
     if y > 0:
-        if worldMap[y-1][x] != 0:
-            return 0
-    if x < currentAIState[WIDTH] - 1:
-        if worldMap[y][x+1] != 0:
-            return 1
-    if y < currentAIState[HEIGHT] - 1:
-        if worldMap[y+1][x] != 0:
-            return 2
-    return 3 # Illegal move if any, over here is not our problem
+        if (worldMap[y-1][x] != WALL):
+            legal = 0
+            if (worldMap[y-1][x] != GHOST):
+                return 0
+    if x < currentAIState[GLOBALS][WIDTH] - 1:
+        if (worldMap[y][x+1] != WALL):
+            legal = 1
+            if (worldMap[y][x+1] != GHOST):
+                return 1
+    if y < currentAIState[GLOBALS][HEIGHT] - 1:
+        if (worldMap[y+1][x] != WALL):
+            legal = 2
+            if (worldMap[y+1][x] != GHOST):
+                return 2
+    if x > 0:
+        if (worldMap[y][x-1] != WALL):
+            legal = 3
+            if (worldMap[y][x-1] != GHOST):
+                return 3
+    return legal # Illegal move, not sure what to return
 
 def Main(initialWorldState, undocumented):
     def AIStepFunction(currentAIState, currentWorldState):
@@ -100,41 +118,30 @@ def Main(initialWorldState, undocumented):
     mapWidth = len(worldMap[0])
 
     initialAIState = []
-    initialAIState.append(0) # No ticks so far
-    initialAIState.append(mapHeight)
-    initialAIState.append(mapWidth)
+    initialAIState.append([]) # Functions should go here
+
+    initialAIState.append([])
+    initialAIState[GLOBALS].append(0) # No ticks so far
+    initialAIState[GLOBALS].append(mapHeight)
+    initialAIState[GLOBALS].append(mapWidth)
+
+    initialAIState.append([])
+    initialAIState[TIMERS].append(127) # Lambda timer
+    initialAIState[TIMERS].append(137) # Lambda eating timer
+    initialAIState[TIMERS].append(130) # Ghost 1 timer
+    initialAIState[TIMERS].append(132) # Ghost 2 timer
+    initialAIState[TIMERS].append(134) # Ghost 3 timer
+    initialAIState[TIMERS].append(136) # Ghost 4 timer
+    initialAIState[TIMERS].append(195) # Ghost 1 fright
+    initialAIState[TIMERS].append(198) # Ghost 2 fright
+    initialAIState[TIMERS].append(201) # Ghost 3 fright
+    initialAIState[TIMERS].append(204) # Ghost 4 fright
     
     return initialAIState, AIStepFunction
 
-def getNewMap(oldMap, move):
-    # This is dumb and will only consider lambdaman
-    # Right now, only used for testing
-    newMap = []
-    for row in oldMap:
-        newMap.append(row[:])
-    x, y = parse(oldMap)[0]
-    newMap[y][x] = EMPTY
-    if move == 0:
-        newMap[y-1][x] = LAMBDAMAN
-    elif move == 1:
-        print newMap, y, x
-        newMap[y][x+1] = LAMBDAMAN
-    elif move == 2:
-        newMap[y+1][x] = LAMBDAMAN
-    else:
-        newMap[y][x-1] = LAMBDAMAN
-    return newMap
-
 if __name__ == '__main__':
     worldMap = [[0, 0, 0], [0, 2, 5]]
-    initialWorldState = [worldMap, None, None, None]
-    initialAIState, AIStepFunction = Main(initialWorldState, None)
-    print initialWorldState, initialAIState
-    currentAIState, bestMove = AIStepFunction(initialAIState, initialWorldState)
-    currentWorldState = [getNewMap(initialWorldState[0], bestMove)]
-    print currentWorldState, currentAIState, bestMove
-    currentAIState, bestMove = AIStepFunction(initialAIState, currentWorldState)
-    currentWorldState = [getNewMap(currentWorldState[0], bestMove)]
-    currentAIState, bestMove = AIStepFunction(initialAIState, currentWorldState)
-    print currentWorldState, currentAIState, bestMove
-
+    t_initialWorldState = (worldMap, (0, (2, 1)), None, None)
+    initialAIState, AIStepFunction = Main(t_initialWorldState, None)
+    currentAIState, bestMove = AIStepFunction(initialAIState, t_initialWorldState)
+    print currentAIState, bestMove
