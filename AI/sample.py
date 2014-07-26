18a73c969c71e0e225203b787db6210ad0b5f3fa
Atmaram Shetye <atmaram.shetye@gmail.com>
2014-07-26 11:43:35 +0530

    Added skeleton for sequence of actions on the AI state

--- AI/sample.py
+++ AI/sample.py
@@ -1,3 +1,19 @@
+# Representing AI state as a list with the following indices
+TICKS = 0
+SCORE = 0
+HEIGHT = 1
+WIDTH = 2
+
+# Map Encoding
+WALL = 0
+EMPTY = 1
+PILL = 2
+POWERPILL = 3
+FRUIT = 4
+LAMBDAMAN = 5
+GHOST = 6
+
+
 def getNewPosition(oldPos, direction):
     if direction == 0: #up
         return (oldPos[0], oldPos[1] - 1)
@@ -15,43 +31,35 @@ def fruit2Valid(ticks):
 
 
 ######## Sequence in which map should be updated ######
-def lambdaGhostMove():
-    pass
+def lambdaGhostMove(currentAIState):
+    return currentAIState
 
-def actions():
-    pass
+def actions(currentAIState):
+    return currentAIState
 
-def eat():
-    pass
+def eat(currentAIState):
+    return currentAIState
 
-def dealWithGhosts():
-    pass
+def dealWithGhosts(currentAIState):
+    return currentAIState
 
-def checkLambdaWin():
-    pass
+def checkLambdaWin(currentAIState):
+    return currentAIState
 
-def checkLambdaLoss():
-    pass
+def checkLambdaLoss(currentAIState):
+    return currentAIState
 
-def incrementTick():
-    pass
+def incrementTick(currentAIState):
+    ticks = currentAIState[TICKS] + 1
+    return [ticks] + currentAIState[1:]
 
 Sequence = (lambdaGhostMove, actions, eat, dealWithGhosts,
         checkLambdaWin, checkLambdaLoss, incrementTick)
 
-# Representing AI state as a list with the following indices
-TICKS = 0
-HEIGHT = 1
-WIDTH = 2
-
-# Map Encoding
-WALL = 0
-EMPTY = 1
-PILL = 2
-POWERPILL = 3
-FRUIT = 4
-LAMBDAMAN = 5
-GHOST = 6
+def makeMove(currentAIState):
+    def f(x, y):
+        return y(x) 
+    return reduce(f, Sequence, currentAIState)
 
 def parse(worldMap):
     ret = []
@@ -73,10 +81,10 @@ def getBestMove(currentAIState, currentWorldState):
     if y > 0:
         if worldMap[y-1][x] != 0:
             return 0
-    if x < currentAIState[2] - 1:
+    if x < currentAIState[WIDTH] - 1:
         if worldMap[y][x+1] != 0:
             return 1
-    if y < currentAIState[1] - 1:
+    if y < currentAIState[HEIGHT] - 1:
         if worldMap[y+1][x] != 0:
             return 2
     return 3 # Illegal move if any, over here is not our problem
@@ -84,7 +92,7 @@ def getBestMove(currentAIState, currentWorldState):
 def Main(initialWorldState, undocumented):
     def AIStepFunction(currentAIState, currentWorldState):
         bestMove = getBestMove(currentAIState, currentWorldState)
-        currentAIState[0] += 1
+        currentAIState = makeMove(currentAIState)
         return currentAIState, bestMove
 
     worldMap, lambdaStatus, ghostStatus, fruitStatus = initialWorldState
@@ -99,8 +107,34 @@ def Main(initialWorldState, undocumented):
     
     return initialAIState, AIStepFunction
 
+def getNewMap(oldMap, move):
+    # This is dumb and will only consider lambdaman
+    newMap = []
+    for row in oldMap:
+        newMap.append(row[:])
+    x, y = parse(oldMap)[0]
+    newMap[y][x] = EMPTY
+    if move == 0:
+        newMap[y-1][x] = LAMBDAMAN
+    elif move == 1:
+        print newMap, y, x
+        newMap[y][x+1] = LAMBDAMAN
+    elif move == 2:
+        newMap[y+1][x] = LAMBDAMAN
+    else:
+        newMap[y][x-1] = LAMBDAMAN
+    return newMap
+
 if __name__ == '__main__':
     worldMap = [[0, 0, 0], [0, 2, 5]]
     initialWorldState = [worldMap, None, None, None]
     initialAIState, AIStepFunction = Main(initialWorldState, None)
-    print AIStepFunction(initialAIState, initialWorldState)
+    print initialWorldState, initialAIState
+    currentAIState, bestMove = AIStepFunction(initialAIState, initialWorldState)
+    currentWorldState = [getNewMap(initialWorldState[0], bestMove)]
+    print currentWorldState, currentAIState, bestMove
+    currentAIState, bestMove = AIStepFunction(initialAIState, currentWorldState)
+    currentWorldState = [getNewMap(currentWorldState[0], bestMove)]
+    currentAIState, bestMove = AIStepFunction(initialAIState, currentWorldState)
+    print currentWorldState, currentAIState, bestMove
+
