51d77e3f64067220cf7b5bd88af5d99e11526621
Shivram Khandeparker <shivramk@gmail.com>
2014-07-28 17:16:15 +0530

    Better ghost tracking in lambdaman5

--- lambdaman5.lisp
+++ lambdaman5.lisp
@@ -1,18 +1,23 @@
 (define (at map ghostlocs fruit x y)
   (set! row (getlistelem map y))
-  (if (contains? ghostlocs (cons x y) cmploc) 6
-      (do 
-        (set! val (getlistelem row x))
-        (if (= val 4)
-          (if (> fruit 0) val 0) val))))
+  (set! ghosts (contains? ghostlocs (cons 0 (cons x y)) cmploc2))
+  (if (atom? ghosts)
+    (do 
+      (set! val (getlistelem row x))
+      (if (= val 4)
+        (if (> fruit 0) val 0) val))
+    (if (= (car ghosts) 1) 7 6)))
 
 (define (contains? list elem cmp)
   (if (atom? list) 0
-    (if (cmp elem (car list)) 1
+    (if (cmp elem (car list)) (car list)
       (contains? (cdr list) elem cmp))))
 
 (define (cmploc l1 l2)
   (and (= (car l1) (car l2)) (= (cdr l1) (cdr l2))))
+
+(define (cmploc2 l1 l2)
+  (and (= (cadr l1) (cadr l2)) (= (cddr l1) (cddr l2))))
 
 (define (min a b)
   (if (< a b) a b))
@@ -95,27 +100,49 @@
   (if (atom? list) list
     (cons (func (car list)) (map (cdr list) func))))
 
-(define (getloc x) (getlistelem x 1))
+(define (getloc x) (cons (car x ) (cadr x)))
 
 (define (cmploc a b) (and (= (car a) (car b)) (= (cdr a) (cdr b))))
 
+(define (item-val v)
+  (cond ((= v 2) 1)
+        ((= v 3) 5)
+        ((= v 4) 10)
+        ((= v 7) 4)
+        (else 0)))
+
 (define (dfs width height map ghostlocs fruit x y vislist depth md)
-  (if (or (or (or (< x 0) (>= x width)) (or (< y 0) (>= y height))) (>= depth md)) 10000
+  (if (or (or (or (< x 0) (>= x width)) (or (< y 0) (>= y height))) (>= depth md)) (+ 10000 depth)
     (do (set! val (at map ghostlocs fruit x y))
-      (if (or (= val 0) (= val 6)) 10000
-        (if (find1 vislist (cons x y) cmploc) 10000
-          (if (or (= val 2) (or (= val 3) (= val 4))) depth
-            (do
-            (set! nv (cons (cons x y) vislist))
-            (set! nd (+ depth 1))
-            (set! scoreleft (dfs width height map ghostlocs fruit (- x 1) y nv nd md))
-            (set! md (min scoreleft md))
-            (set! scoreright (dfs width height map ghostlocs fruit (+ x 1) y nv nd md))
-            (set! md (min scoreright md))
-            (set! scoreup (dfs width height map ghostlocs fruit x (- y 1) nv nd md))
-            (set! md (min scoreup md))
-            (set! scoredown (dfs width height map ghostlocs fruit x (+ y 1) nv nd md))
-            (set! md (min scoredown md)) md)))))))
+      (if (= val 0) 10000
+        (if (= val 6) 100000
+          (if (find1 vislist (cons x y) cmploc) (+ 10000 depth)
+            (if (or (= val 2) (or (= val 3) (or (= val 4) (= val 7)))) (- depth (item-val val))
+              (do
+                (set! nv (cons (cons x y) vislist))
+                (set! nd (+ depth 1))
+                (set! scoreleft (dfs width height map ghostlocs fruit (- x 1) y nv nd md))
+                (set! md (min scoreleft md))
+                (set! scoreright (dfs width height map ghostlocs fruit (+ x 1) y nv nd md))
+                (set! md (min scoreright md))
+                (set! scoreup (dfs width height map ghostlocs fruit x (- y 1) nv nd md))
+                (set! md (min scoreup md))
+                (set! scoredown (dfs width height map ghostlocs fruit x (+ y 1) nv nd md))
+                (set! md (min scoredown md)) md))))))))
+
+(define (augmentlocs m)
+  (if (atom? m) m
+    (do
+      (set! current (car m))
+      (set! stat (car current))
+      (set! x (cadr current))
+      (set! y (cddr current))
+      (cons current 
+        (cons (cons stat (cons (- x 1) y))
+              (cons (cons stat (cons (+ x 1) y))
+                    (cons (cons stat (cons x (- y 1)))
+                          (cons (cons stat (cons x (+ y 1))) (augmentlocs (cdr m))))))))))
+
 
 (define (make_move worldstate)
   (set! worldmap (gettupleelem worldstate 0))
@@ -127,16 +154,17 @@
   (set! y (gettupleelem lpos 1))
   (set! dir (gettupleelem lambdastate 2))
   (set! ghoststate (gettupleelem worldstate 2))
-  (set! ghostlocs (map ghoststate getloc))
+  (set! ghostlocs (augmentlocs (augmentlocs (map ghoststate getloc))))
+  ; (debug ghostlocs)
   (set! oppdir (mod (+ 2 dir) 4))
   (set! fruit (gettupleelem worldstate 3))
-  (debug fruit)
-  (set! depth 17)
+  ; (debug fruit)
+  (set! depth 16)
   (set! sleft  (dfs width height worldmap ghostlocs fruit (- x 1) y 0 1 depth))
   (set! sright (dfs width height worldmap ghostlocs fruit (+ x 1) y 0 1 depth))
   (set! sup    (dfs width height worldmap ghostlocs fruit x (- y 1) 0 1 depth))
   (set! sdown  (dfs width height worldmap ghostlocs fruit x (+ y 1) 0 1 depth))
-  (debug (cons sup (cons sright (cons sdown (cons sleft 0)))))
+  ; (debug (cons sup (cons sright (cons sdown (cons sleft 0)))))
   (minidx (cons sup (cons sright (cons sdown (cons sleft 0))))))
 
 (define (step aistate worldstate) 
